b5a80cd6ed832b9c424193cc6546b4c3a5d7efa8
my-personal-repos <85361787+my-personal-repos@users.noreply.github.com>
2021-10-01 17:16:43 +0530

    new list added

--- test.py
+++ test.py
@@ -17,3 +17,10 @@ for index, url in enumerate(urls):
     if res.status_code != 200:
         print(f"{index}: {url}")
         input()
+        
+for index, url in enumerate([]):
+    print(f"Checking page {index}...")
+    res = requests.get(url)
+    if res.status_code != 200:
+        print(f"{index}: {url}")
+        input()
